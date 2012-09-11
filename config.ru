98ed99df39f1a1c78bc5df5e174e1bd3223e93d0
Jonathan Bryant <jbryant@rallydev.com>
2012-09-10 19:20:27 -0600

    Fixing god config and rackup file

--- config.ru
+++ config.ru
@@ -1,4 +1,4 @@
-$:.unshift "lib"
+$:.unshift File.expand_path(File.join(File.dirname(__FILE__), "lib"))
 
 require 'gdash'
 
--- config/god.rb
+++ config/god.rb
@@ -1,15 +1,16 @@
 INSTANCES = 3
 BASE_PORT = 3000
+RACKUP = File.expand_path(File.join(File.dirname(__FILE__), "..", "config.ru"))
 
-INSTANCES.each do |instance|
+INSTANCES.times do |instance|
   log_file = File.expand_path(File.join(File.dirname(__FILE__), "..", "log", "gdash.#{instance}.log"))
 
   God.watch do |watch|
     watch.name = "GDash #{instance}"
     watch.group = "gdash"
-    watch.start = "bundle exec thin --rackup config.ru --port #{BASE_PORT + instance} --log #{log_file}.log start"
+    watch.start = "bundle exec thin --rackup #{RACKUP} --port #{BASE_PORT + instance} --log #{log_file} start"
     watch.behavior :clean_pid_file
     watch.keepalive
-    watch.log log_file
+    watch.log = log_file
   end
 end
