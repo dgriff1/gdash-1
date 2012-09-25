ffb3e747290f7247092f24cd06bc0b61a3a908e5
Jonathan Bryant <jbryant@rallydev.com>
2012-09-25 15:31:37 -0600

    Moved Hadoop and friends into a separate dashboard

--- dashboards/boulder.rb
+++ dashboards/boulder.rb
@@ -3,231 +3,236 @@ GDash::Dashboard.define :boulder do |boulder|
   boulder.description = "Servers in Boulder"
   boulder.ganglia_host = "http://bld-mon-03/ganglia"
 
-  boulder.dashboard :dev_hadoop do |dashboard|
-    dashboard.title = "Development Hadoop"
-    dashboard.description = "Development Hadoop cluster (Mac Pros)"
+  boulder.dashboard :hadoop do |hadoop|
+    hadoop.title = "Hadoop Clusters"
+    hadoop.description = "Hadoop and Friends in Boulder"
 
-    dashboard.section :title => "System", :width => 2 do |section|
-      section.ganglia_report :title => "CPU Usage" do |ganglia_report|
-        ganglia_report.report = "cpu_report"
-        ganglia_report.cluster = "Boulder Hadoop"
-        ganglia_report.size = "xlarge"
+    hadoop.dashboard :dev_hadoop do |dashboard|
+      dashboard.title = "Development"
+      dashboard.description = "Development Hadoop cluster (Mac Pros)"
+
+      dashboard.section :title => "System", :width => 2 do |section|
+        section.ganglia_report :title => "CPU Usage" do |ganglia_report|
+          ganglia_report.report = "cpu_report"
+          ganglia_report.cluster = "Boulder Hadoop"
+          ganglia_report.size = "xlarge"
+        end
+
+        section.ganglia_report :title => "Network Usage" do |ganglia_report|
+          ganglia_report.report = "network_report"
+          ganglia_report.cluster = "Boulder Hadoop"
+          ganglia_report.size = "xlarge"
+        end
       end
 
-      section.ganglia_report :title => "Network Usage" do |ganglia_report|
-        ganglia_report.report = "network_report"
-        ganglia_report.cluster = "Boulder Hadoop"
-        ganglia_report.size = "xlarge"
-      end
-    end
-
-    dashboard.section :title => "Kafka", :width => 3 do |section|
-      section.ganglia_graph :title => "Messages In" do |graph|
-        graph.hosts = "bld-hadoop-0[123]"
-        graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerAllTopicStat.MessagesIn"
-        graph.type = :stack
-        graph.size = "xlarge"
-      end
-
-      section.ganglia_graph :title => "Bytes In" do |graph|
-        graph.hosts = "bld-hadoop-0[123]"
-        graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerAllTopicStat.BytesIn"
-        graph.type = :stack
-        graph.size = "xlarge"
-      end
-
-      section.ganglia_graph :title => "Bytes Out" do |ganglia_graph|
-        ganglia_graph.hosts = "bld-hadoop-0[123]"
-        ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerAllTopicStat.BytesIn"
-        ganglia_graph.type = :stack
-        ganglia_graph.size = "xlarge"
-      end
-    end
-
-    ["prod-a2", "prod-beacon", "prod-server", "trial-server"].each do |topic|
-      dashboard.section :title => "Topic: #{topic}", :width => 3 do |section|
-        section.ganglia_graph :title => "Messages In" do |ganglia_graph|
-          ganglia_graph.hosts = "bld-hadoop-0[123]"
-          ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.MessagesIn"
-          ganglia_graph.type = :stack
-          ganglia_graph.size = "xlarge"
+      dashboard.section :title => "Kafka", :width => 3 do |section|
+        section.ganglia_graph :title => "Messages In" do |graph|
+          graph.hosts = "bld-hadoop-0[123]"
+          graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerAllTopicStat.MessagesIn"
+          graph.type = :stack
+          graph.size = "xlarge"
         end
 
         section.ganglia_graph :title => "Bytes In" do |graph|
           graph.hosts = "bld-hadoop-0[123]"
-          graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.BytesIn"
+          graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerAllTopicStat.BytesIn"
           graph.type = :stack
           graph.size = "xlarge"
         end
 
         section.ganglia_graph :title => "Bytes Out" do |ganglia_graph|
           ganglia_graph.hosts = "bld-hadoop-0[123]"
-          ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.BytesOut"
+          ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerAllTopicStat.BytesIn"
           ganglia_graph.type = :stack
           ganglia_graph.size = "xlarge"
         end
       end
-    end
-  end
 
-  boulder.dashboard :boulder_hadoop do |dashboard|
-    dashboard.title = "Hadoop"
-    dashboard.description = "Hadoop Cluster"
+      ["prod-a2", "prod-beacon", "prod-server", "trial-server"].each do |topic|
+        dashboard.section :title => "Topic: #{topic}", :width => 3 do |section|
+          section.ganglia_graph :title => "Messages In" do |ganglia_graph|
+            ganglia_graph.hosts = "bld-hadoop-0[123]"
+            ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.MessagesIn"
+            ganglia_graph.type = :stack
+            ganglia_graph.size = "xlarge"
+          end
 
-    dashboard.section :title => "System", :width => 2 do |section|
-      section.ganglia_report :title => "CPU Usage" do |ganglia_report|
-        ganglia_report.report = "cpu_report"
-        ganglia_report.cluster = "Boulder Hadoop Revisited"
-        ganglia_report.size = "xlarge"
-      end
+          section.ganglia_graph :title => "Bytes In" do |graph|
+            graph.hosts = "bld-hadoop-0[123]"
+            graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.BytesIn"
+            graph.type = :stack
+            graph.size = "xlarge"
+          end
 
-      section.ganglia_report :title => "Network Usage" do |ganglia_report|
-        ganglia_report.report = "network_report"
-        ganglia_report.cluster = "Boulder Hadoop Revisited"
-        ganglia_report.size = "xlarge"
-      end
-    end
-  end
-
-  boulder.dashboard :boulder_kafka do |dashboard|
-    dashboard.title = "Kafka"
-    dashboard.description = "Kafka cluster"
-
-    dashboard.section :title => "System", :width => 3 do |section|
-      section.ganglia_report :title => "CPU Usage" do |ganglia_report|
-        ganglia_report.report = "cpu_report"
-        ganglia_report.cluster = "Boulder Kafka"
-        ganglia_report.size = "xlarge"
-      end
-
-      section.ganglia_report :title => "Network Usage" do |ganglia_report|
-        ganglia_report.report = "network_report"
-        ganglia_report.cluster = "Boulder Kafka"
-        ganglia_report.size = "xlarge"
-      end
-
-      section.ganglia_graph :title => "Disk Free" do |ganglia_graph|
-        ganglia_graph.hosts = "bld-kafka-0[123]"
-        ganglia_graph.metrics = "disk_free$"
-        ganglia_graph.vertical_label = "GB Free"
-        ganglia_graph.size = "xlarge"
+          section.ganglia_graph :title => "Bytes Out" do |ganglia_graph|
+            ganglia_graph.hosts = "bld-hadoop-0[123]"
+            ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.BytesOut"
+            ganglia_graph.type = :stack
+            ganglia_graph.size = "xlarge"
+          end
+        end
       end
     end
 
-    dashboard.section :title => "Kafka", :width => 4 do |section|
-      section.ganglia_graph :title => "Messages In" do |ganglia_graph|
-        ganglia_graph.hosts = "bld-kafka-0[123]"
-        ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerAllTopicStat.MessagesIn"
-        ganglia_graph.type = :stack
-      end
+    hadoop.dashboard :boulder_hadoop do |dashboard|
+      dashboard.title = "Hadoop"
+      dashboard.description = "Hadoop Cluster"
 
-      section.ganglia_graph :title => "Heap Used" do |ganglia_graph|
-        ganglia_graph.hosts = "bld-kafka-0[123]"
-        ganglia_graph.metrics = "sun.management.MemoryImpl.Memory.HeapMemoryUsage.used"
-      end
+      dashboard.section :title => "System", :width => 2 do |section|
+        section.ganglia_report :title => "CPU Usage" do |ganglia_report|
+          ganglia_report.report = "cpu_report"
+          ganglia_report.cluster = "Boulder Hadoop Revisited"
+          ganglia_report.size = "xlarge"
+        end
 
-      section.ganglia_graph :title => "Disk Writes" do |ganglia_graph|
-        ganglia_graph.hosts = "bld-kafka-0[123]"
-        ganglia_graph.metrics = "diskstat_sda_write_bytes_per_sec"
-      end
-
-      section.ganglia_graph :title => "Disk Reads" do |ganglia_graph|
-        ganglia_graph.hosts = "bld-kafka-0[123]"
-        ganglia_graph.metrics = "diskstat_sda_read_bytes_per_sec"
+        section.ganglia_report :title => "Network Usage" do |ganglia_report|
+          ganglia_report.report = "network_report"
+          ganglia_report.cluster = "Boulder Hadoop Revisited"
+          ganglia_report.size = "xlarge"
+        end
       end
     end
 
-    ["prod-a2", "prod-beacon", "prod-server-start", "prod-server-end", "trial-server-start", "trial_server-end", "dev-server-start", "dev-server-end", "test-server-start", "test-server-end", "loadtest-server-start", "loadtest-server-end"].each do |topic|
-      dashboard.section :title => "Topic: #{topic}", :width => 3 do |section|
+    hadoop.dashboard :boulder_kafka do |dashboard|
+      dashboard.title = "Kafka"
+      dashboard.description = "Kafka cluster"
+
+      dashboard.section :title => "System", :width => 3 do |section|
+        section.ganglia_report :title => "CPU Usage" do |ganglia_report|
+          ganglia_report.report = "cpu_report"
+          ganglia_report.cluster = "Boulder Kafka"
+          ganglia_report.size = "xlarge"
+        end
+
+        section.ganglia_report :title => "Network Usage" do |ganglia_report|
+          ganglia_report.report = "network_report"
+          ganglia_report.cluster = "Boulder Kafka"
+          ganglia_report.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Disk Free" do |ganglia_graph|
+          ganglia_graph.hosts = "bld-kafka-0[123]"
+          ganglia_graph.metrics = "disk_free$"
+          ganglia_graph.vertical_label = "GB Free"
+          ganglia_graph.size = "xlarge"
+        end
+      end
+
+      dashboard.section :title => "Kafka", :width => 4 do |section|
         section.ganglia_graph :title => "Messages In" do |ganglia_graph|
           ganglia_graph.hosts = "bld-kafka-0[123]"
-          ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.MessagesIn"
+          ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerAllTopicStat.MessagesIn"
           ganglia_graph.type = :stack
-          ganglia_graph.size = "xlarge"
         end
 
-        section.ganglia_graph :title => "Bytes In" do |ganglia_graph|
+        section.ganglia_graph :title => "Heap Used" do |ganglia_graph|
           ganglia_graph.hosts = "bld-kafka-0[123]"
-          ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.BytesIn"
-          ganglia_graph.type = :stack
-          ganglia_graph.size = "xlarge"
+          ganglia_graph.metrics = "sun.management.MemoryImpl.Memory.HeapMemoryUsage.used"
         end
 
-        section.ganglia_graph :title => "Bytes Out" do |ganglia_graph|
+        section.ganglia_graph :title => "Disk Writes" do |ganglia_graph|
           ganglia_graph.hosts = "bld-kafka-0[123]"
-          ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.BytesOut"
-          ganglia_graph.type = :stack
-          ganglia_graph.size = "xlarge"
+          ganglia_graph.metrics = "diskstat_sda_write_bytes_per_sec"
+        end
+
+        section.ganglia_graph :title => "Disk Reads" do |ganglia_graph|
+          ganglia_graph.hosts = "bld-kafka-0[123]"
+          ganglia_graph.metrics = "diskstat_sda_read_bytes_per_sec"
+        end
+      end
+
+      ["prod-a2", "prod-beacon", "prod-server-start", "prod-server-end", "trial-server-start", "trial_server-end", "dev-server-start", "dev-server-end", "test-server-start", "test-server-end", "loadtest-server-start", "loadtest-server-end"].each do |topic|
+        dashboard.section :title => "Topic: #{topic}", :width => 3 do |section|
+          section.ganglia_graph :title => "Messages In" do |ganglia_graph|
+            ganglia_graph.hosts = "bld-kafka-0[123]"
+            ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.MessagesIn"
+            ganglia_graph.type = :stack
+            ganglia_graph.size = "xlarge"
+          end
+
+          section.ganglia_graph :title => "Bytes In" do |ganglia_graph|
+            ganglia_graph.hosts = "bld-kafka-0[123]"
+            ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.BytesIn"
+            ganglia_graph.type = :stack
+            ganglia_graph.size = "xlarge"
+          end
+
+          section.ganglia_graph :title => "Bytes Out" do |ganglia_graph|
+            ganglia_graph.hosts = "bld-kafka-0[123]"
+            ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.BytesOut"
+            ganglia_graph.type = :stack
+            ganglia_graph.size = "xlarge"
+          end
         end
       end
     end
-  end
 
-  boulder.dashboard :boulder_zookeeper do |dashboard|
-    dashboard.title = "ZooKeeper"
-    dashboard.description = "ZooKeeper cluster"
+    hadoop.dashboard :boulder_zookeeper do |dashboard|
+      dashboard.title = "ZooKeeper"
+      dashboard.description = "ZooKeeper cluster"
 
-    dashboard.section :title => "System", :width => 2 do |section|
-      section.ganglia_report :title => "CPU Usage" do |ganglia_report|
-        ganglia_report.report = "cpu_report"
-        ganglia_report.cluster = "Boulder Zookeeper"
-        ganglia_report.size = "xlarge"
-      end
-
-      section.ganglia_report :title => "Network Usage" do |ganglia_report|
-        ganglia_report.report = "network_report"
-        ganglia_report.cluster = "Boulder Zookeeper"
-        ganglia_report.size = "xlarge"
-      end
-    end
-
-    dashboard.section :title => "Cluster", :width => 2 do |section|
-      section.ganglia_graph :title => "Heap Used" do |report|
-        report.hosts = "bld-zookeeper-0[123]"
-        report.metrics = "sun.management.MemoryImpl.Memory.HeapMemoryUsage.used"
-        report.size = "xlarge"
-      end
-
-      section.ganglia_graph :title => "Disk Writes" do |ganglia_graph|
-        ganglia_graph.hosts = "bld-zookeeper-0[123]"
-        ganglia_graph.metrics = "diskstat_sda_write_bytes_per_sec"
-        ganglia_graph.size = "xlarge"
-      end
-
-      section.ganglia_graph :title => "Outstanding Requests" do |ganglia_graph|
-        ganglia_graph.hosts = "bld-zookeeper-0[123]"
-        ganglia_graph.metrics = "org.apache.zookeeper.server.ConnectionBean.OutstandingRequests"
-        ganglia_graph.size = "xlarge"
-      end
-
-      section.ganglia_graph :title => "Latency" do |ganglia_graph|
-        ganglia_graph.hosts = "bld-zookeeper-0[123]"
-        ganglia_graph.metrics = "org.apache.zookeeper.server.ConnectionBean.LastLatency"
-        ganglia_graph.size = "xlarge"
-      end
-    end
-
-    ["bld-zookeeper-01", "bld-zookeeper-02", "bld-zookeeper-03"].each do |host|
-      dashboard.section :title => "Host: #{host}", :width => 4 do |section|
-        section.ganglia_report do |graph|
-          graph.report = "zk_connection_throughput_report"
-          graph.cluster = "Boulder Zookeeper"
-          graph.host = "#{host}.f4tech.com"
-          graph.size = "xlarge"
-        end
-
-        section.ganglia_report do |ganglia_report|
-          ganglia_report.report = "zk_connection_latency_report"
+      dashboard.section :title => "System", :width => 2 do |section|
+        section.ganglia_report :title => "CPU Usage" do |ganglia_report|
+          ganglia_report.report = "cpu_report"
           ganglia_report.cluster = "Boulder Zookeeper"
-          ganglia_report.host = "#{host}.f4tech.com"
           ganglia_report.size = "xlarge"
         end
 
-        section.ganglia_report do |ganglia_report|
-          ganglia_report.report = "zk_leader_follower_latency_report"
+        section.ganglia_report :title => "Network Usage" do |ganglia_report|
+          ganglia_report.report = "network_report"
           ganglia_report.cluster = "Boulder Zookeeper"
-          ganglia_report.host = "#{host}.f4tech.com"
           ganglia_report.size = "xlarge"
+        end
+      end
+
+      dashboard.section :title => "Cluster", :width => 2 do |section|
+        section.ganglia_graph :title => "Heap Used" do |report|
+          report.hosts = "bld-zookeeper-0[123]"
+          report.metrics = "sun.management.MemoryImpl.Memory.HeapMemoryUsage.used"
+          report.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Disk Writes" do |ganglia_graph|
+          ganglia_graph.hosts = "bld-zookeeper-0[123]"
+          ganglia_graph.metrics = "diskstat_sda_write_bytes_per_sec"
+          ganglia_graph.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Outstanding Requests" do |ganglia_graph|
+          ganglia_graph.hosts = "bld-zookeeper-0[123]"
+          ganglia_graph.metrics = "org.apache.zookeeper.server.ConnectionBean.OutstandingRequests"
+          ganglia_graph.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Latency" do |ganglia_graph|
+          ganglia_graph.hosts = "bld-zookeeper-0[123]"
+          ganglia_graph.metrics = "org.apache.zookeeper.server.ConnectionBean.LastLatency"
+          ganglia_graph.size = "xlarge"
+        end
+      end
+
+      ["bld-zookeeper-01", "bld-zookeeper-02", "bld-zookeeper-03"].each do |host|
+        dashboard.section :title => "Host: #{host}", :width => 4 do |section|
+          section.ganglia_report do |graph|
+            graph.report = "zk_connection_throughput_report"
+            graph.cluster = "Boulder Zookeeper"
+            graph.host = "#{host}.f4tech.com"
+            graph.size = "xlarge"
+          end
+
+          section.ganglia_report do |ganglia_report|
+            ganglia_report.report = "zk_connection_latency_report"
+            ganglia_report.cluster = "Boulder Zookeeper"
+            ganglia_report.host = "#{host}.f4tech.com"
+            ganglia_report.size = "xlarge"
+          end
+
+          section.ganglia_report do |ganglia_report|
+            ganglia_report.report = "zk_leader_follower_latency_report"
+            ganglia_report.cluster = "Boulder Zookeeper"
+            ganglia_report.host = "#{host}.f4tech.com"
+            ganglia_report.size = "xlarge"
+          end
         end
       end
     end
@@ -283,7 +288,7 @@ GDash::Dashboard.define :boulder do |boulder|
       dashboard.dashboard :"#{name}_kafka_ingestors" do |kafka_ingestors|
         kafka_ingestors.title = "Kafka Ingestors"
 
-        kafka_ingestors.section :title => "Lags", :width => 2 do |section|
+        kafka_ingestors.section :title => "Lag in Bytes", :width => 2 do |section|
           instance[:topics].each do |topic|
             section.ganglia_graph :title => topic do |ganglia_graph|
               ganglia_graph.hosts = instance[:host]
@@ -307,7 +312,7 @@ GDash::Dashboard.define :boulder do |boulder|
          :subscriber,        :subscription, :time,
          :tps_view,          :trace,        :unhandled_exception,
          :user_agent,        :wsapi].each do |dimension|
-          dimensions.section :title => "#{dimension.to_s.titleize} Dimension", :width => 3 do |section|
+          dimensions.section :title => dimension.to_s.titleize, :width => 3 do |section|
             section.ganglia_graph :title => "#{dimension.to_s.titleize} Times" do |ganglia_graph|
               ganglia_graph.hosts = instance[:host]
               ganglia_graph.metrics = "^#{dimension}_dimension_.*_time.duration.mean"
