fd38293d0b0227e524fce5d730e58ee402cfefcc
Jonathan Bryant <jbryant@rallydev.com>
2012-10-02 11:28:49 -0600

    Added Denver and Seattle Kafka and Zookeeper Dashboards

--- dashboards/prod.rb
+++ dashboards/prod.rb
@@ -280,6 +280,178 @@
 
     end # End of ALM Dashboard
 
+    dashboard.dashboard :"#{site[:prefix]}_kafka" do |kafka|
+      kafka.title = "Kafka"
+      kafka.description = "Kafka cluster"
+
+      kafka.section :title => "System", :width => 4 do |section|
+        section.ganglia_report :title => "Load Average" do |ganglia_report|
+          ganglia_report.report = "load_report"
+          ganglia_report.cluster = "Kafka"
+          ganglia_report.size = "large"
+        end
+
+        section.ganglia_report :title => "CPU Usage" do |ganglia_report|
+          ganglia_report.report = "cpu_report"
+          ganglia_report.cluster = "Kafka"
+          ganglia_report.size = "large"
+        end
+
+        section.ganglia_report :title => "Network Usage" do |ganglia_report|
+          ganglia_report.report = "network_report"
+          ganglia_report.cluster = "Kafka"
+          ganglia_report.size = "large"
+        end
+
+        section.ganglia_report :title => "Memory Usage" do |ganglia_report|
+          ganglia_report.report = "mem_report"
+          ganglia_report.cluster = "Kafka"
+          ganglia_report.size = "large"
+        end
+      end
+
+      kafka.section :title => "Kafka", :width => 3 do |section|
+        section.ganglia_graph :title => "Messages In" do |ganglia_graph|
+          ganglia_graph.hosts = "#{site[:prefix]}-kafka-0[123]"
+          ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerAllTopicStat.MessagesIn"
+          ganglia_graph.type = :stack
+          ganglia_graph.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Heap Used" do |ganglia_graph|
+          ganglia_graph.hosts = "#{site[:prefix]}-kafka-0[123]"
+          ganglia_graph.metrics = "sun.management.MemoryImpl.Memory.HeapMemoryUsage.used"
+          ganglia_graph.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Disk Writes" do |ganglia_graph|
+          ganglia_graph.hosts = "#{site[:prefix]}-kafka-0[123]"
+          ganglia_graph.metrics = "diskstat_sda_write_bytes_per_sec"
+          ganglia_graph.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Disk Reads" do |ganglia_graph|
+          ganglia_graph.hosts = "#{site[:prefix]}-kafka-0[123]"
+          ganglia_graph.metrics = "diskstat_sda_read_bytes_per_sec"
+          ganglia_graph.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Disk Free" do |ganglia_graph|
+          ganglia_graph.hosts = "#{site[:prefix]}-kafka-0[123]"
+          ganglia_graph.metrics = "disk_free$"
+          ganglia_graph.vertical_label = "GB Free"
+          ganglia_graph.size = "xlarge"
+        end
+      end
+
+      ["prod-a2", "prod-beacon", "prod-server-start", "prod-server-end", "trial-server-start", "trial_server-end"].each do |topic|
+        kafka.section :title => "Topic: #{topic}", :width => 3 do |section|
+          section.ganglia_graph :title => "Messages In" do |ganglia_graph|
+            ganglia_graph.hosts = "#{site[:prefix]}-kafka-0[123]"
+            ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.MessagesIn"
+            ganglia_graph.type = :stack
+            ganglia_graph.size = "xlarge"
+          end
+
+          section.ganglia_graph :title => "Bytes In" do |ganglia_graph|
+            ganglia_graph.hosts = "#{site[:prefix]}-kafka-0[123]"
+            ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.BytesIn"
+            ganglia_graph.type = :stack
+            ganglia_graph.size = "xlarge"
+          end
+
+          section.ganglia_graph :title => "Bytes Out" do |ganglia_graph|
+            ganglia_graph.hosts = "#{site[:prefix]}-kafka-0[123]"
+            ganglia_graph.metrics = "kafka.server.BrokerTopicStat.kafka.BrokerTopicStat.#{topic}.BytesOut"
+            ganglia_graph.type = :stack
+            ganglia_graph.size = "xlarge"
+          end
+        end
+      end
+    end
+
+    dashboard.dashboard :"#{site[:prefix]}_zookeeper" do |zookeeper|
+      zookeeper.title = "ZooKeeper"
+      zookeeper.description = "ZooKeeper cluster"
+
+      zookeeper.section :title => "System", :width => 4 do |section|
+        section.ganglia_report :title => "Load Average" do |ganglia_report|
+          ganglia_report.report = "load_report"
+          ganglia_report.cluster = "Zookeeper"
+          ganglia_report.size = "large"
+        end
+
+        section.ganglia_report :title => "CPU Usage" do |ganglia_report|
+          ganglia_report.report = "cpu_report"
+          ganglia_report.cluster = "Zookeeper"
+          ganglia_report.size = "large"
+        end
+
+        section.ganglia_report :title => "Network Usage" do |ganglia_report|
+          ganglia_report.report = "network_report"
+          ganglia_report.cluster = "Zookeeper"
+          ganglia_report.size = "large"
+        end
+
+        section.ganglia_report :title => "Memory Usage" do |ganglia_report|
+          ganglia_report.report = "mem_report"
+          ganglia_report.cluster = "Zookeeper"
+          ganglia_report.size = "large"
+        end
+      end
+
+      zookeeper.section :title => "Cluster", :width => 2 do |section|
+        section.ganglia_graph :title => "Heap Used" do |report|
+          report.hosts = "#{site[:prefix]}-zookeeper-0[123]"
+          report.metrics = "sun.management.MemoryImpl.Memory.HeapMemoryUsage.used"
+          report.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Disk Writes" do |ganglia_graph|
+          ganglia_graph.hosts = "#{site[:prefix]}-zookeeper-0[123]"
+          ganglia_graph.metrics = "diskstat_sda_write_bytes_per_sec"
+          ganglia_graph.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Outstanding Requests" do |ganglia_graph|
+          ganglia_graph.hosts = "#{site[:prefix]}-zookeeper-0[123]"
+          ganglia_graph.metrics = "org.apache.zookeeper.server.ConnectionBean.OutstandingRequests"
+          ganglia_graph.size = "xlarge"
+        end
+
+        section.ganglia_graph :title => "Latency" do |ganglia_graph|
+          ganglia_graph.hosts = "#{site[:prefix]}-zookeeper-0[123]"
+          ganglia_graph.metrics = "org.apache.zookeeper.server.ConnectionBean.LastLatency"
+          ganglia_graph.size = "xlarge"
+        end
+      end
+
+      ["#{site[:prefix]}-zookeeper-01", "#{site[:prefix]}-zookeeper-02", "#{site[:prefix]}-zookeeper-03"].each do |host|
+        zookeeper.section :title => "Host: #{host}", :width => 4 do |section|
+          section.ganglia_report do |graph|
+            graph.report = "zk_connection_throughput_report"
+            graph.cluster = "Zookeeper"
+            graph.host = "#{host}.rally.prod"
+            graph.size = "xlarge"
+          end
+
+          section.ganglia_report do |ganglia_report|
+            ganglia_report.report = "zk_connection_latency_report"
+            ganglia_report.cluster = "Zookeeper"
+            ganglia_report.host = "#{host}.rally.prod"
+            ganglia_report.size = "xlarge"
+          end
+
+          section.ganglia_report do |ganglia_report|
+            ganglia_report.report = "zk_leader_follower_latency_report"
+            ganglia_report.cluster = "Zookeeper"
+            ganglia_report.host = "#{host}.rally.prod"
+            ganglia_report.size = "xlarge"
+          end
+        end
+      end
+    end
+
   end # End of site Dashboard
 
 end # End of site each block
