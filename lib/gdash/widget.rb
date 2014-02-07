c7950ba252cd842854010d10a12f773fcaed9b93
Brian Dupras <bdupras@rallydev.com>
2014-02-07 10:57:28 -0700

    added help display and fixed lightboxes

--- lib/gdash/widget.rb
+++ lib/gdash/widget.rb
@@ -1,5 +1,6 @@
 module GDash
   class Widget < Base
+    attr :help
     class << self
       def inherited subclass
         Section.send :define_method, subclass.name.to_s.demodulize.underscore do |*args, &block|
